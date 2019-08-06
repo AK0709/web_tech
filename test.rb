5f130403e1cfbb30cf965ba324c8f5abeafa4367
atsuo kubota <atsuo.kubota0709@gmail.com>
2019-08-06 11:50:40 +0900

    second commit to hand out 1008061150

---
+++ test.rb
@@ -0,0 +1,26 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/add', WEBrick::HTTPServlet::ERBHandler, 'add.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+# server.mount('/add.cgi', WEBrick::HTTPServlet::CGIHandler, 'add.rb')
+
+
+server.start
